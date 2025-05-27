796889822b186d7545fd05b8d30f45612f1f6602
SweetWuXiaoMei <sweetwuxiaomei@qq.com>
2025-05-27 11:50:29 +0800

    忽略case

--- hibernate-ucp/src/test/java/org/hibernate/test/ucp/UCPTransactionIsolationConfigTest.java
+++ hibernate-ucp/src/test/java/org/hibernate/test/ucp/UCPTransactionIsolationConfigTest.java
@@ -1,6 +1,7 @@
 package org.hibernate.test.ucp;
 
 import org.hibernate.community.dialect.AltibaseDialect;
+import org.hibernate.community.dialect.GaussDBDialect;
 import org.hibernate.dialect.SybaseDialect;
 import org.hibernate.dialect.TiDBDialect;
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
@@ -11,6 +12,7 @@ import org.hibernate.testing.orm.junit.SkipForDialect;
 @SkipForDialect(dialectClass = SybaseDialect.class, matchSubTypes = true, reason = "The jTDS driver doesn't implement Connection#getNetworkTimeout() so this fails")
 @SkipForDialect(dialectClass = TiDBDialect.class, matchSubTypes = true, reason = "Doesn't support SERIALIZABLE isolation")
 @SkipForDialect(dialectClass = AltibaseDialect.class, matchSubTypes = true, reason = "Altibase cannot change isolation level in autocommit mode")
+@SkipForDialect(dialectClass = GaussDBDialect.class, reason = "GaussDB does not support SERIALIZABLE isolation")
 public class UCPTransactionIsolationConfigTest extends BaseTransactionIsolationConfigTest{
 	@Override
 	protected ConnectionProvider getConnectionProviderUnderTest() {
